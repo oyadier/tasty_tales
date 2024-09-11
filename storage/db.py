46f5aadce2dfefc90048e883fcd1d1778eae0bbe
Oyadier Robert Mensah <oyadierrobertmensah@gmail.com>
2024-09-11 14:54:41 +0100

    Add mongod and routers

---
+++ storage/db.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A connector to the Mongod Client"""
+
+from pymongo import MongoClient
+
+mongo_url = 'mongod://localhost:27017/'
+
+def isConnected():
+    client = MongoClient()
+    db = client['tasty_tales']
+    collection = db['recipes']
+    return  collection
+ 
+                                                      
