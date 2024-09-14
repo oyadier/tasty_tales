5d3368a212f0b34dd5735bdac2fef3ea917f776b
Eric <ericgeorge4477@gmail.com>
2024-09-15 02:57:59 +0300

    Comment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,3 +1,29 @@
+// {
+//   "created_at": "Tue Sep 10 13:23:36 2024",
+//   "rep_name": "Jollof Rice",
+//   "author": "Kukwa Suma",
+//   "ingredients": [
+//       "Rice",
+//       "Tomatoes",
+//       "Pepper",
+//       "Onions",
+//       "Vegetable oil",
+//       "Chicken",
+//       "Stock",
+//       "Spices"
+//   ],
+//   "region": "West Africa (Popular in Nigeria)",
+//   "cooking_method": "Stewing",
+//   "preparation_time_minutes": 60,
+//   "instructions": [
+//       "1. Blend tomatoes, pepper, and onions to make a paste.",
+//       "2. Fry the paste in vegetable oil until the oil separates.",
+//       "3. Add chicken stock and spices.",
+//       "4. Add rice and cook on low heat until tender.",
+//       "5. Serve with fried chicken."
+//   ]
+// },
+
 function Home() {
   return <div>Home</div>;
 }
