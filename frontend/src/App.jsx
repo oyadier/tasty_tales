3df23bff95b4bd54f2102ae40b08ce3c2c859059
Eric <ericgeorge4477@gmail.com>
2024-09-15 02:49:29 +0300

    Modify App routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,19 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./App.css";
 import AuthLayout from "./layouts/AuthLayout";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import MasterLayout from "./layouts/MasterLayout";
 import Home from "./pages/Home";
+import CreateRecipe from "./pages/CreateRecipe";
 
 const routes = createBrowserRouter([
   {
-    path: "/",
+    path: "/login",
     element: <AuthLayout />,
     children: [
       { index: true, element: <Login /> },
@@ -17,11 +22,13 @@ const routes = createBrowserRouter([
     ],
   },
   {
-    path: "/home",
+    path: "/",
     element: <MasterLayout />,
     children: [
       { index: true, element: <Home /> },
       { path: "home", element: <Home /> },
+      { path: "*", element: <Navigate to="/home" replace /> },
+      { path: "create-recipe", element: <CreateRecipe /> },
     ],
   },
 ]);
