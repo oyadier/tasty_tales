542d7f6d77d9b00db4a9112f30a328925100f4c7
Eric <ericgeorge4477@gmail.com>
2024-09-21 04:39:48 +0300

    update app routing structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,19 @@ import {
 import "./App.css";
 import AuthLayout from "./layouts/AuthLayout";
 import Login from "./pages/Login";
-import Register from "./pages/Register";
 import MasterLayout from "./layouts/MasterLayout";
 import Home from "./pages/Home";
 import CreateRecipe from "./pages/CreateRecipe";
+import Recipes from "./pages/Recipes";
+import SignUp from "./pages/SignUp";
 
 const routes = createBrowserRouter([
   {
-    path: "/login",
+    path: "/",
     element: <AuthLayout />,
     children: [
-      { index: true, element: <Login /> },
       { path: "login", element: <Login /> },
-      { path: "register", element: <Register /> },
+      { path: "signup", element: <SignUp /> },
     ],
   },
   {
@@ -29,6 +29,7 @@ const routes = createBrowserRouter([
       { path: "home", element: <Home /> },
       { path: "*", element: <Navigate to="/home" replace /> },
       { path: "create-recipe", element: <CreateRecipe /> },
+      { path: "my-recipes", element: <Recipes /> },
     ],
   },
 ]);
