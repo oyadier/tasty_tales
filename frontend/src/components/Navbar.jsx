7f9883811c024243fa2077cb68bf7093c52d7c6a
Eric <ericgeorge4477@gmail.com>
2024-09-23 02:28:33 +0300

    modify Navbar UI and integrate useCookies hook

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,6 @@ import {
   IconButton,
   Typography,
   Menu,
-  Container,
   Avatar,
   Button,
   Tooltip,
@@ -14,72 +13,80 @@ import {
   styled,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import SearchIcon from "@mui/icons-material/Search";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
-const StyledAppBar = styled(AppBar)({
+// Custom styles for the components
+const StyledAppBar = styled(AppBar)(({ theme }) => ({
   position: "fixed",
-  backgroundColor: "white",
-  height: "5em",
+  backgroundColor: "#fff",
   boxShadow: "none",
-  maxWidth: "800px",
   width: "100%",
-  margin: "0 auto",
-  right: "auto",
-});
+  top: "0",
+  zIndex: 1100,
+}));
 
-const StyledToolbar = styled(Toolbar)({
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   display: "flex",
-  justifyContent: "center",
+  justifyContent: "space-between",
   alignItems: "center",
-});
+  gap: "20px",
+  padding: "0 24px",
+  [theme.breakpoints.up("md")]: {
+    padding: "0 48px",
+  },
+}));
 
-const StyledTypography = styled(Typography)({
-  marginRight: "16px",
+const StyledLogo = styled(Link)(({ theme }) => ({
   fontFamily: "monospace",
   fontWeight: 700,
   letterSpacing: ".3rem",
-  color: "black",
+  color: "#000",
   textDecoration: "none",
-  display: "none",
-  "@media (min-width: 800px)": {
-    display: "flex",
-    justifyContent: "space-evenly",
+  fontSize: "24px", // Adjusted for larger screens
+  display: "inline-flex", // Ensure it only takes the width of the content
+  alignItems: "center", // Align text to center vertically (optional)
+  justifyContent: "center", // Align text to center horizontally (optional)
+  [theme.breakpoints.down("sm")]: {
+    fontSize: "20px", // Smaller font for mobile screens
   },
-});
+}));
 
-const StyledMenu = styled(Menu)({
-  display: "none",
-  "@media (max-width: 899px)": {
-    display: "block",
-  },
-});
-
-const StyledButton = styled(Button)({
-  margin: "16px 2em",
-  color: "black",
-  display: "block",
-  fontSize: "18px",
+const StyledButton = styled(Button)(({ theme }) => ({
+  margin: "0 1em",
+  color: "#000",
+  textTransform: "none",
+  fontSize: "18px", // Slightly larger font for clarity
+  fontWeight: 500,
   "&:hover": {
-    backgroundColor: "#fcfcfc",
+    backgroundColor: "#f7f7f7",
   },
-});
+}));
 
-const StyledSearchIcon = styled(SearchIcon)({
-  fontSize: 35,
-  color: "black",
-  paddingRight: "0.8em",
-  display: "none",
-  "@media (min-width: 900px)": {
-    display: "block",
+const StyledLink = styled(Link)(({ theme }) => ({
+  margin: "0 1em",
+  color: "#000",
+  fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  textTransform: "none",
+  fontSize: "18px", // Slightly larger font for clarity
+  fontWeight: 500,
+  textDecoration: "none",
+  "&:hover": {
+    textDecoration: "underline",
   },
-});
+}));
 
-const StyledAvatar = styled(Avatar)({});
+const StyledAvatar = styled(Avatar)(({ theme }) => ({
+  width: 40,
+  height: 40,
+  cursor: "pointer",
+}));
 
-const pages = ["Recipe Page", "AboutUs"];
-const settings = ["Profile", "Logout"];
+const sections = ["Home", "About Us", "Contact Us"];
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies(["token"]);
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -88,6 +95,7 @@ const Navbar = () => {
   };
 
   const handleCloseNavMenu = () => {
+    navigate("/");
     setAnchorElNav(null);
   };
 
@@ -95,130 +103,107 @@ const Navbar = () => {
     setAnchorElUser(event.currentTarget);
   };
 
-  const handleCloseUserMenu = () => {
+  const handleCloseUserMenu = (section) => {
+    navigate(section);
     setAnchorElUser(null);
   };
 
-  return (
-    <Box sx={{ display: "flex", justifyContent: "center" }}>
-      <StyledAppBar>
-        <Container maxWidth="900px" sx={{ margin: "0 auto" }}>
-          <StyledToolbar disableGutters>
-            <StyledTypography
-              variant="h6"
-              noWrap
-              component="a"
-              href="#app-bar-with-responsive-menu"
-            >
-              LOGO
-            </StyledTypography>
+  const handleLogout = () => {
+    // Clear the token from cookies
+    removeCookie("token", { path: "/" });
+    navigate("/login");
+  };
 
-            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="black"
-              >
-                <MenuIcon />
-              </IconButton>
-              <StyledMenu
+  return (
+    <nav>
+      <StyledAppBar position="static">
+        <StyledToolbar disableGutters>
+          {/* Logo */}
+          <StyledLogo to="/home">TASTY TALES</StyledLogo>
+
+          {/* Home Section Links (Desktop) */}
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {sections.map((section) => (
+              <StyledLink key={section}>{section}</StyledLink>
+            ))}
+          </Box>
+          {cookies.token ? (
+            /* User Avatar Menu */
+            <Box sx={{ flexGrow: 0 }}>
+              <Tooltip title="Open options">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <StyledAvatar alt="Profile Avatar" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: "45px" }}
+                disableScrollLock
                 id="menu-appbar"
-                anchorEl={anchorElNav}
+                anchorEl={anchorElUser}
                 anchorOrigin={{
-                  vertical: "bottom",
-                  horizontal: "left",
+                  vertical: "top",
+                  horizontal: "right",
                 }}
                 keepMounted
                 transformOrigin={{
                   vertical: "top",
-                  horizontal: "left",
+                  horizontal: "right",
                 }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
+                open={Boolean(anchorElUser)}
+                onClose={() => setAnchorElUser(null)}
               >
-                {pages.map((page) => (
-                  <MenuItem key={page} onClick={handleCloseNavMenu}>
-                    <Typography sx={{ textAlign: "center" }}>{page}</Typography>
-                  </MenuItem>
-                ))}
-              </StyledMenu>
+                <MenuItem onClick={() => handleCloseUserMenu("/my-recipes")}>
+                  <Typography>{"my recipes"}</Typography>
+                </MenuItem>
+                <MenuItem onClick={handleLogout}>
+                  <Typography>{"logout"}</Typography>
+                </MenuItem>
+              </Menu>
             </Box>
-            <Typography
-              variant="h5"
-              noWrap
-              component="a"
-              href=""
-              sx={{
-                mx: 2,
-                display: { xs: "flex", md: "none" },
-                flexGrow: 1,
-                fontFamily: "monospace",
-                fontWeight: 700,
-                letterSpacing: ".3rem",
-                color: "black",
-                textDecoration: "none",
-              }}
-            >
-              LOGO
-            </Typography>
-            <Box
-              sx={{
-                flexGrow: 1,
-                display: { xs: "none", md: "flex" },
-                justifyContent: "center",
-                mx: { xs: "0", md: "4" },
-                ml: "-10px",
-              }}
-            >
-              {pages.map((page) => (
-                <StyledButton key={page} onClick={handleCloseNavMenu}>
-                  {page}
-                </StyledButton>
-              ))}
+          ) : (
+            /* Login Button  */
+            <Box>
+              <StyledLink onClick={() => navigate("/login")}>Login</StyledLink>
             </Box>
-            {/* <Box>
-          <StyledSearchIcon/>
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <StyledAvatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
+          )}
+
+          {/* Mobile Menu (Hamburger) */}
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+              sx={{ color: "#000", padding: 0 }} // Remove padding
+            >
+              <MenuIcon />
+            </IconButton>
             <Menu
-              sx={{ mt: '45px' }}
               id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
+              anchorEl={anchorElNav}
+              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
               keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
+              transformOrigin={{ vertical: "top", horizontal: "center" }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{ display: { xs: "block", md: "none" } }}
             >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
+              {sections.map((section) => (
+                <MenuItem key={section} onClick={handleCloseNavMenu}>
+                  <Typography>{section}</Typography>
                 </MenuItem>
               ))}
             </Menu>
-          </Box> */}
-
-            <Box>
-              <StyledButton>Login</StyledButton>
-            </Box>
-          </StyledToolbar>
-        </Container>
+          </Box>
+        </StyledToolbar>
       </StyledAppBar>
-    </Box>
+    </nav>
   );
 };
 
