e0959f0bca833f7de3a7f9f89e4e8096313658dd
Eric <ericgeorge4477@gmail.com>
2024-09-23 03:30:03 +0300

    Recipe (#25)
    * implement login and sign up pages with React Hook Form validation
    
    * update app routing structure
    
    * add files
    
    * modify Navbar UI and integrate useCookies hook
    
    * Move Navbar component to MasterLayout
    
    * complete Login and Signup API integration with axios

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { styled, Container, Grid2, Typography, Box } from '@mui/material';
+import React, { useState, useEffect } from "react";
+import { styled, Container, Grid2, Typography, Box } from "@mui/material";
 
 function List() {
   const [recipes, setRecipes] = useState([]);
@@ -9,9 +9,9 @@ function List() {
   useEffect(() => {
     const fetchRecipes = async () => {
       try {
-        const response = await fetch('https://dummyjson.com/recipes');
+        const response = await fetch("https://dummyjson.com/recipes");
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
         const data = await response.json();
         setRecipes(data.recipes || []);
@@ -25,33 +25,34 @@ function List() {
     fetchRecipes();
   }, []);
 
-  if (loading) return <Typography sx={{ textAlign: 'center' }}>Loading...</Typography>;
-  if (error) return <Typography sx={{ textAlign: 'center' }}>Error: {error}</Typography>;
+  if (loading)
+    return <Typography sx={{ textAlign: "center" }}>Loading...</Typography>;
+  if (error)
+    return <Typography sx={{ textAlign: "center" }}>Error: {error}</Typography>;
 
   return (
-    <Box sx={{ maxWidth: '800px', mx: 'auto', p: 2 }}>
+    <Box sx={{ maxWidth: "800px", mx: "auto", p: 2 }}>
       <Grid2
         container
-        spacing={{xs:2,md: 3 }}
-        sx={{ display: 'flex', justifyContent: 'center', flexWrap: 'wrap' }}
+        spacing={{ xs: 2, md: 3 }}
+        sx={{ display: "flex", justifyContent: "center", flexWrap: "wrap" }}
       >
         {recipes.length > 0 ? (
           recipes.map((recipe) => (
             <Grid2
-              item
               key={recipe.id}
               display="flex"
               flexDirection="column"
               alignItems="center"
-              sx={{ width: { md: '250px', sm: '200px', xs: '150px' } }} 
+              sx={{ width: { md: "250px", sm: "200px", xs: "150px" } }}
             >
               <img
                 src={recipe.image}
                 alt={recipe.name}
                 style={{
-                  width: '100%',
-                  height: 'auto',
-                  maxWidth: '240px',
+                  width: "100%",
+                  height: "auto",
+                  maxWidth: "240px",
                 }}
               />
               <Typography variant="subtitle2" align="center" sx={{ mt: 1 }}>
